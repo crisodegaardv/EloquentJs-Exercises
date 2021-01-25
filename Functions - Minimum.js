e33f58824a41db6145331f50252b2a40aba8afc7
cristianode <71045698+cristianode@users.noreply.github.com>
2021-01-25 19:51:22 -0300

    first commit

---
+++ Functions - Minimum.js
@@ -0,0 +1,9 @@
+//Eloquent JavaScript - Funciones, ejercicio 1: Minimum
+
+const minimum = (num1, num2) => {
+    let min;
+    min = Math.min(num1, num2);
+    return min;
+}
+
+console.log(minimum(30, 50))
